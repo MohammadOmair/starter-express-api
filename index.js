bf54a396ce1a8168674645ccffc3cfde571f61af
MohammadOmair <130234239+MohammadOmair@users.noreply.github.com>
2023-05-09 21:45:52 +0500

    Configured paths for portal win

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
 const Xray = require('x-ray');
 const x = Xray();
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
-const MyModel = require('./Models/predictions');
-
-const MONGODB_URI = 'mongodb+srv://admin-umair:test123@cluster0.xg387ne.mongodb.net/PortalDB';
-
-
-
-app.use(express.json());
+ 
 app.use(cors());
-
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-
-const ConnectToMongo = () => {
-    mongoose
-        .connect(MONGODB_URI, {
-            useNewUrlParser: true,
-        })
-        .then(() => {
-            console.log("DB Connection Succesful");
-        })
-        .catch((e) => {
-            console.log("Something went wrong while connecting to DB: \n", e);
-        });
-};
-ConnectToMongo()
 
 function getData(url, count) {
     return new Promise((resolve, reject) => {
@@ -43,7 +17,6 @@ function getData(url, count) {
             odds: 'td.oddPredBook',
             result: '.imgCorrect img@title',
             lg: 'td.iconLega img@alt',
-            img: 'td.iconLega img@class'
 
         }])((error, results) => {
             if (error) {
@@ -55,7 +28,7 @@ function getData(url, count) {
     });
 }
 
-app.get('/home/:date/:count', (req, res) => { //1X2
+app.get('/home/:date/:count', (req, res) => {
 
     const url = `https://www.bettingclosed.com/predictions/date-matches/${req.params.date}`;
 
@@ -65,28 +38,7 @@ app.get('/home/:date/:count', (req, res) => { //1X2
         .catch(error => { res.status(500).send(error) });
 });
 
-app.get('/underover/:date/:count', (req, res) => { //   under/over
-
-    const url = `https://www.bettingclosed.com/predictions/date-matches/${req.params.date}/bet-type/under-over`;
-
-    getData(url, req.params.count)
-        .then(results => { res.status(200).send(results) })
-        .catch(error => { res.status(500).send(error) });
-
-
-});
-
-app.get('/finalscores/:date/:count', (req, res) => { //   final scores
-
-    const url = `https://www.bettingclosed.com/predictions/date-matches/${req.params.date}/bet-type/correct-scores`;
-
-    getData(url, req.params.count)
-        .then(results => { res.status(200).send(results) })
-        .catch(error => { res.status(500).send(error) });
-});
-
-
-app.get('/mixed/:count', (req, res) => { //
+app.get('/mixed/:count', (req, res) => {
 
     const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/mixed';
 
@@ -97,10 +49,9 @@ app.get('/mixed/:count', (req, res) => { //
 
 });
 
+app.get('/underover/:count', (req, res) => {
 
-app.get('/golnogol/:count', (req, res) => {
-
-    const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/gol-nogol';
+    const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/under-over';
 
     getData(url, req.params.count)
         .then(results => { res.status(200).send(results) })
@@ -109,37 +60,24 @@ app.get('/golnogol/:count', (req, res) => {
 
 });
 
+app.get('/golnogol/:count', (req, res) => {
 
-app.post('/admin/save', async (req, res) => {
-    try {
-        const { day, firstTeam, league, odds, secondTeam, time, tip, img } = req.body;
-        const myDocument = new MyModel({ day, firstTeam, league, odds, secondTeam, time, tip, img });
-        await myDocument.save();
-        res.status(200).json({ success: true });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Failed to save document' }); // Respond with an error message
-    }
+    const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/gol-nogol';
+ 
+    getData(url, req.params.count)
+        .then(results => { res.status(200).send(results) })
+        .catch(error => { res.status(500).send(error) });
+
+
 });
 
-app.delete('/admin/clear', async (req, res) => {
-    try {
-        await MyModel.deleteMany(); // Delete all documents in the collection
-        res.status(200).json({ success: true }); // Respond with a success message
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Failed to clear collection' }); // Respond with an error message
-    }
-});
+app.get('/finalscores/:count', (req, res) => {
 
-app.get('/admin/data', async (req, res) => {
-    try {
-        const documents = await MyModel.find(); // Find all documents in the collection
-        res.status(200).json({ success: true, data: documents }); // Respond with the documents
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Failed to retrieve documents' }); // Respond with an error message
-    }
+    const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/correct-scores';
+
+    getData(url, req.params.count)
+        .then(results => { res.status(200).send(results) })
+        .catch(error => { res.status(500).send(error) });
 });
 
 
