b1f55ae7b6f147d861ea8fcc7681ec361de7bf9d
MohammadOmair <130234239+MohammadOmair@users.noreply.github.com>
2023-10-03 20:47:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,81 +6,49 @@ const cors = require('cors');
  
 app.use(cors());
 
-function getData(url, count) {
-    return new Promise((resolve, reject) => {
-        x(url, `table#myTable tbody tr:nth-child(-n+${count})`, [{
-            dateTime: 'td.dataMt',
-            team_b: 'td.teamBmatch',
-            team_a: 'td.teamAmatch',
-            score: 'td.resultMt',
-            prediction: 'td.predMt a',
-            odds: 'td.oddPredBook',
-            result: '.imgCorrect img@title',
-            lg: 'td.iconLega img@alt',
 
-        }])((error, results) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(results);
-            }
-        });
+app.use(express.json());
+const fetch = require('node-fetch');
+
+// Define the URL and authentication credentials
+let url = '';
+const apiKey = 'bdcabb21-a43e-4dcf-989e-fc0f5e7f9340';
+
+
+
+app.get('/:number', (req, res) => {
+
+
+  let arr = [];
+  url = 'https://api.company-information.service.gov.uk/company/' + req.params.number + '/filing-history';
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        console.log(`HTTP Error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      data?.items?.forEach(item => {
+        if (item.type === 'SH02') {
+          arr.push(item);
+        }
+      })
+
+      res.status(200).send({ success: true, data: arr })
+    })
+    .catch(error => {
+      // Handle errors here
+      res.status(200).send({ success: false, error })
     });
-}
-
-app.get('/home/:date/:count', (req, res) => {
-
-    const url = `https://www.bettingclosed.com/predictions/date-matches/${req.params.date}`;
-
-
-    getData(url, req.params.count)
-        .then(results => { res.status(200).send(results) })
-        .catch(error => { res.status(500).send(error) });
-});
-
-app.get('/mixed/:count', (req, res) => {
-
-    const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/mixed';
-
-    getData(url, req.params.count)
-        .then(results => { res.status(200).send(results) })
-        .catch(error => { res.status(500).send(error) });
-
 
 });
-
-app.get('/underover/:count', (req, res) => {
-
-    const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/under-over';
-
-    getData(url, req.params.count)
-        .then(results => { res.status(200).send(results) })
-        .catch(error => { res.status(500).send(error) });
-
-
-});
-
-app.get('/golnogol/:count', (req, res) => {
-
-    const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/gol-nogol';
- 
-    getData(url, req.params.count)
-        .then(results => { res.status(200).send(results) })
-        .catch(error => { res.status(500).send(error) });
-
-
-});
-
-app.get('/finalscores/:count', (req, res) => {
-
-    const url = 'https://www.bettingclosed.com/predictions/date-matches/today/bet-type/correct-scores';
-
-    getData(url, req.params.count)
-        .then(results => { res.status(200).send(results) })
-        .catch(error => { res.status(500).send(error) });
-});
-
-
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
