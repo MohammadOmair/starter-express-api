5eeb66707d4dc3ba7a6afc59d158f62665e7c955
MohammadOmair <130234239+MohammadOmair@users.noreply.github.com>
2023-10-03 21:38:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
-const Xray = require('x-ray');
-const x = Xray();
 const express = require('express');
 const app = express();
 const cors = require('cors');
- 
+const axios = require('axios'); // Import Axios
 app.use(cors());
-
-
 app.use(express.json());
-const fetch = require('node-fetch');
 
 // Define the URL and authentication credentials
-let url = '';
 const apiKey = 'bdcabb21-a43e-4dcf-989e-fc0f5e7f9340';
 
+app.get('/:number', async (req, res) => {
+  try {
+    let arr = [];
+    const apiUrl = `https://api.company-information.service.gov.uk/company/${req.params.number}/filing-history`;
 
+    // Set up Axios configuration
+    const axiosConfig = {
+      headers: {
+        'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`,
+      },
+    };
 
-app.get('/:number', (req, res) => {
+    // Make the GET request using Axios
+    const response = await axios.get(apiUrl, axiosConfig);
 
+    if (response.status !== 200) {
+      console.log(`HTTP Error! Status: ${response.status}`);
+      res.status(200).send({ success: false, error: `HTTP Error! Status: ${response.status}` });
+      return;
+    }
 
-  let arr = [];
-  url = 'https://api.company-information.service.gov.uk/company/' + req.params.number + '/filing-history';
+    const data = response.data;
 
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`,
-    },
-  })
-    .then(response => {
-      if (!response.ok) {
-        console.log(`HTTP Error! Status: ${response.status}`);
+    data?.items?.forEach(item => {
+      if (item.type === 'SH02') {
+        arr.push(item);
       }
-      return response.json();
-    })
-    .then(data => {
-      data?.items?.forEach(item => {
-        if (item.type === 'SH02') {
-          arr.push(item);
-        }
-      })
-
-      res.status(200).send({ success: true, data: arr })
-    })
-    .catch(error => {
-      // Handle errors here
-      res.status(200).send({ success: false, error })
     });
 
+    res.status(200).send({ success: true, data: arr });
+  } catch (error) {
+    // Handle errors here
+    console.error('Error:', error);
+    res.status(200).send({ success: false, error });
+  }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
