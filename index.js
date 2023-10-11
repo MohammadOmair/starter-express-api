8dcc4325fc989eb0b18eead7fc33987aec8a4662
MohammadOmair <130234239+MohammadOmair@users.noreply.github.com>
2023-10-12 00:24:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,121 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 const axios = require('axios'); // Import Axios
+const realEstateData = require('./Models/predictions');
 app.use(cors());
 app.use(express.json());
 
-// Define the URL and authentication credentials
-const apiKey = 'bdcabb21-a43e-4dcf-989e-fc0f5e7f9340';
+// const apiKey = 'bdcabb21-a43e-4dcf-989e-fc0f5e7f9340';
+const url = "mongodb+srv://admin-umair:test123@cluster0.xg387ne.mongodb.net/SampleDB";
+const documentId = '6526d7dff2f1b0a565cd7bfe';
 
-app.get('/:number', async (req, res) => {
+const ConnectToMongo = async () => {
   try {
-    let arr = [];
-    const apiUrl = `https://api.company-information.service.gov.uk/company/${req.params.number}/filing-history`;
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+    });
+    console.log("DB Connection Successful");
+  } catch (error) {
+    console.log("Something went wrong Umair ! : ", error);
+  }
+};
 
-    // Set up Axios configuration
-    const axiosConfig = {
-      headers: {
-        'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`,
-      },
-    };
+async function findAndDecrementCount(documentId) {
+  let document;
+  try {
+    document = await realEstateData.findById(documentId);
 
-    // Make the GET request using Axios
-    const response = await axios.get(apiUrl, axiosConfig);
-
-    if (response.status !== 200) {
-      console.log(`HTTP Error! Status: ${response.status}`);
-      res.status(200).send({ success: false, error: `HTTP Error! Status: ${response.status}` });
-      return;
+    if (!document) {
+      console.log('Document not found');
+      return -1;
     }
 
-    const data = response.data;
+    document.countId--;
+    await document.save();
 
-    data?.items?.forEach(item => {
-      if (item.type === 'SH02') {
-        arr.push(item);
-      }
-    });
-
-    res.status(200).send({ success: true, data: arr });
+    console.log('Document found and countId decremented:', document);
   } catch (error) {
-    // Handle errors here
-    console.error('Error:', error);
-    res.status(200).send({ success: false, error });
+    console.error('Error finding and decrementing countId:', error);
   }
+
+  return document.countId;
+
+}
+
+
+
+app.get('/', async (req, res) => {
+  ConnectToMongo()
+  const count = await findAndDecrementCount(documentId)
+  console.log(count)
+  if (count <= 0) return res.send({ message: 'condition matched' })
+
+  axios.get('https://giddy-cyan-squirrel.cyclic.app/')
+
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// try {
+//   let arr = [];
+//   const apiUrl = `https://api.company-information.service.gov.uk/company/${req.params.number}/filing-history`;
+
+//   // Set up Axios configuration
+//   const axiosConfig = {
+//     headers: {
+//       'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`,
+//     },
+//   };
+
+//   // Make the GET request using Axios
+//   const response = await axios.get(apiUrl, axiosConfig);
+
+//   if (response.status !== 200) {
+//     console.log(`HTTP Error! Status: ${response.status}`);
+//     res.status(200).send({ success: false, error: `HTTP Error! Status: ${response.status}` });
+//     return;
+//   }
+
+//   const data = response.data;
+
+//   data?.items?.forEach(item => {
+//     if (item.type === 'SH02') {
+//       arr.push(item);
+//     }
+//   });
+
+//   res.status(200).send({ success: true, data: arr });
+// } catch (error) {
+//   // Handle errors here
+//   console.error('Error:', error);
+//   res.status(200).send({ success: false, error });
+// }
